95ac9c03c1507cbf707a9e705bfb33297e12e9ba
Dimzmiks555 <53266916+Dimzmiks555@users.noreply.github.com>
2022-11-16 17:48:47 +0300

    'repair_works'

--- client/components/screens/Transport/RepairWorksScreen/RepairWorksScreen.tsx
+++ client/components/screens/Transport/RepairWorksScreen/RepairWorksScreen.tsx
@@ -1,9 +1,15 @@
+import { Button } from '@mui/material'
+import Link from 'next/link'
 import React from 'react'
+import PageHeader from '../../../shared/PageHeader/PageHeader'
 
 const RepairWorksScreen = () => {
   return (
     <div>
-        <h1>Ремонтные работы</h1>
+        <PageHeader>
+            <h1>Ремонтные работы</h1>
+            <Link href='/transport/all/create'><Button sx={{color: 'white'}} variant="contained">Добавить</Button></Link>
+        </PageHeader>
     </div>
   )
 }
